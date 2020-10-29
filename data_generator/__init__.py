55a982395e389502428e3df1ff30d277e859e6d6
hezhongyu <hezhongyu0@hotmail.com>
2020-10-29 11:55:10 +0800

    add lorenz generator

---
+++ data_generator/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/10/28 20:24
+# @Author  : Zhongyu
+# @File    : __init__.py.py
---
+++ temp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/10/29 10:07
+# @Author  : Zhongyu
+# @File    : temp.py
